44857fde9aa1b5d8a90a13a6c61dd16b373e931d
DEV NAND NAIR <devnandnair2@gmail.com>
2023-10-22 01:41:14 +0530

    commit11

--- anime/anime/urls.py
+++ anime/anime/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('', include('myapp.urls')),
+      path('favicon.ico', lambda request: HttpResponse(status=204)),
+
 ]
+
+
+   
